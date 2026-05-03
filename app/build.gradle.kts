ea4d0bd7c60ff1428a736beaa0b4761a81f42680
RenanLM <renanlm691@gmail.com>
2026-05-02 22:03:22 -0300

    feat: add Task entity and configure Room, Navigation and ViewModel dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("kotlin-kapt")
 }
 
 android {
@@ -57,4 +58,16 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Room Database
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+    // Navigation Compose (Para navegar entre a lista e os detalhes)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    // ViewModel Compose (Para a arquitetura MVVM)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
 }
